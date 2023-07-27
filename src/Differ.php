5facf7aebe8f937879a372c72224e92c72ca9f58
Николай Рябинин <rnn2002@mail.ru>
2023-07-27 16:58:14 +0300

    Modify src/Differ.php

--- src/Differ.php
+++ src/Differ.php
@@ -2,7 +2,48 @@
 
 namespace Differ\Differ;
 
-function genDiff(string $firstFile, string $secondFile, string $format)
+function genDiff(string $pathToFile1, string $pathToFile2, string $formatName): string
 {
+    $contentOfFile1 = file_get_contents($pathToFile1);
+    $contentOfFile2 = file_get_contents($pathToFile2);
 
+    $parsedContentOfFile1 = json_decode($contentOfFile1, true);
+    $parsedContentOfFile2 = json_decode($contentOfFile2, true);
+
+    $allUniqueKeys = array_unique(array_merge(array_keys($parsedContentOfFile1), array_keys($parsedContentOfFile2)));
+    sort($allUniqueKeys);
+
+    $flag = '';
+    $differ = [];
+
+    foreach ($allUniqueKeys as $uniqueKey) {
+        if (
+            array_key_exists($uniqueKey, $parsedContentOfFile1)
+            && array_key_exists($uniqueKey, $parsedContentOfFile2)
+        ) {
+            if ($parsedContentOfFile1[$uniqueKey] === $parsedContentOfFile2[$uniqueKey]) {
+                $flag = ' ';
+                $differ[] = "  {$flag} {$uniqueKey}: {$$parsedContentOfFile1[$uniqueKey]}";
+            } else {
+                $flag = '-';
+                $differ[] = "  {$flag} {$uniqueKey}: {$$parsedContentOfFile1[$uniqueKey]}";
+                $flag = '+';
+                $differ[] = "  {$flag} {$uniqueKey}: {$$parsedContentOfFile2[$uniqueKey]}";
+            }
+        } elseif (
+            array_key_exists($uniqueKey, $parsedContentOfFile1)
+            && !array_key_exists($uniqueKey, $parsedContentOfFile2)
+        ) {
+            $flag = '-';
+            $differ[] = "  {$flag} {$uniqueKey}: {$$parsedContentOfFile1[$uniqueKey]}";
+        } elseif (
+            !array_key_exists($uniqueKey, $parsedContentOfFile1)
+            && array_key_exists($uniqueKey, $parsedContentOfFile2)
+        ) {
+            $flag = '+';
+            $differ[] = "  {$flag} {$uniqueKey}: {$$parsedContentOfFile2[$uniqueKey]}";
+        }
+    }
+    $result = implode("\n", $differ);
+    return "{\n{$result}\n}";
 }
