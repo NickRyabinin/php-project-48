0eb6e27d7a37c8d871ca496e0e1778a0967aa50d
Николай Рябинин <rnn2002@mail.ru>
2023-08-01 19:40:58 +0300

    Modify src/Formatters/Stylish.php

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -4,14 +4,41 @@ namespace Differ\Formatters\Stylish;
 
 function stylishFormat(array $diff): string
 {
-    // var_export($diff);
-    // $result = implode("\n", $diff);
-    // return "{\n{$result}\n}";
-    return var_export($diff);
+    $formattedDiff = makeStringFromDiff($diff);
+    return "{\n{$formattedDiff}\n}";
 }
 
-function makeStringFromDiff(array $diff, int $level = 0): string
+function makeStringFromDiff(array $diff): string
 {
+    $stringifiedDiff = [];
+
+    foreach ($diff as $node) {
+        echo "\nNode:\n";
+        var_export($node);
+        echo "\n";
+        $status = $node['status'];
+        $key = $node['key'];
+        $value1 = $node['value1'];
+        $value2 = $node['value2'];
+        $stringifiedValue1 = (is_array($value1)) ? makeStringFromDiff($value1) : stringifyValue($value1);
+
+        switch ($status) {
+            case 'nested':
+            case 'same':
+                $stringifiedDiff[] = "    {$key}: {$stringifiedValue1}";
+                break;
+            case 'added':
+                $stringifiedDiff[] = "  + {$key}: {$stringifiedValue1}";
+                break;
+            case 'removed':
+                $stringifiedDiff[] = "  - {$key}: {$stringifiedValue1}";
+                break;
+            case 'updated':
+                $stringifiedValue2 = (is_array($value2)) ? makeStringFromDiff($value2) : stringifyValue($value2);
+                $stringifiedDiff[] = "  - {$key}: {$stringifiedValue1}\n  + {$key}: {$stringifiedValue2}";
+        }
+    }
+    return implode("\n", $stringifiedDiff);
 }
 
 function stringifyValue(mixed $value): mixed
