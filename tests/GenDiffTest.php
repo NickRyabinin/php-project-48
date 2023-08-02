281b067628e92d11c3fb779f796a7aa3a3b31ef6
Николай Рябинин <rnn2002@mail.ru>
2023-08-02 16:50:30 +0300

    Modify tests/GenDiffTest.php - add plain test case

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -25,13 +25,25 @@ class GenDiffTest extends TestCase
         $fixture1 = $this->getPathToFixture('file3.json');
         $fixture2 = $this->getPathToFixture('file4.json');
         $actual = genDiff($fixture1, $fixture2, 'stylish');
-        $expected = file_get_contents($this->getPathToFixture('expectedNested'));
+        $expected = file_get_contents($this->getPathToFixture('expectedStylish'));
         $this->assertEquals($expected, $actual);
 
         $fixture1 = $this->getPathToFixture('file3.yaml');
         $fixture2 = $this->getPathToFixture('file4.yaml');
         $actual = genDiff($fixture1, $fixture2, 'stylish');
-        $expected = file_get_contents($this->getPathToFixture('expectedNested'));
+        $expected = file_get_contents($this->getPathToFixture('expectedStylish'));
+        $this->assertEquals($expected, $actual);
+
+        $fixture1 = $this->getPathToFixture('file3.json');
+        $fixture2 = $this->getPathToFixture('file4.json');
+        $actual = genDiff($fixture1, $fixture2, 'plain');
+        $expected = file_get_contents($this->getPathToFixture('expectedPlain'));
+        $this->assertEquals($expected, $actual);
+
+        $fixture1 = $this->getPathToFixture('file3.yaml');
+        $fixture2 = $this->getPathToFixture('file4.yaml');
+        $actual = genDiff($fixture1, $fixture2, 'plain');
+        $expected = file_get_contents($this->getPathToFixture('expectedPlain'));
         $this->assertEquals($expected, $actual);
     }
 
