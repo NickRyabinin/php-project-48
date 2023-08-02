99f8c6c874f1c95a683e83c32ebf47e859dc11f3
Николай Рябинин <rnn2002@mail.ru>
2023-08-02 16:56:41 +0300

    Modify src/Formatters.php - add plain format case

--- src/Formatters.php
+++ src/Formatters.php
@@ -3,11 +3,13 @@
 namespace Differ\Formatters;
 
 use function Differ\Formatters\Stylish\stylishFormat;
+use function Differ\Formatters\Plain\plainFormat;
 
-function makeFormat($diff, $formatName)
+function makeFormat(array $diff, string $formatName): string
 {
     return match ($formatName) {
         'stylish' => stylishFormat($diff),
+        'plain' => plainFormat($diff),
         default => exit("Unknown format '{$formatName}'!\n")
     };
 }
