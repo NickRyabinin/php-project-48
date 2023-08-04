4848986efa1cd8032e18fa82d593e6c7d075b0e0
Николай Рябинин <rnn2002@mail.ru>
2023-08-04 16:00:41 +0300

    Modify tests/GenDiffTest.php - add assertion for Json format

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -45,6 +45,18 @@ class GenDiffTest extends TestCase
         $actual = genDiff($fixture1, $fixture2, 'plain');
         $expected = file_get_contents($this->getPathToFixture('expectedPlain'));
         $this->assertEquals($expected, $actual);
+
+        $fixture1 = $this->getPathToFixture('file3.json');
+        $fixture2 = $this->getPathToFixture('file4.json');
+        $actual = genDiff($fixture1, $fixture2, 'json');
+        $expected = file_get_contents($this->getPathToFixture('expectedJson'));
+        $this->assertEquals($expected, $actual);
+
+        $fixture1 = $this->getPathToFixture('file3.yaml');
+        $fixture2 = $this->getPathToFixture('file4.yaml');
+        $actual = genDiff($fixture1, $fixture2, 'json');
+        $expected = file_get_contents($this->getPathToFixture('expectedJson'));
+        $this->assertEquals($expected, $actual);
     }
 
     private function getPathToFixture($fixtureName)
