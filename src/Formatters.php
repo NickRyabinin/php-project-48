387dee1fded1b3f6bdd82b479e0bf985d7df146a
Николай Рябинин <rnn2002@mail.ru>
2023-08-04 16:03:21 +0300

    Modify src/Formatters.php - add Json formatter case

--- src/Formatters.php
+++ src/Formatters.php
@@ -4,12 +4,14 @@ namespace Differ\Formatters;
 
 use function Differ\Formatters\Stylish\stylishFormat;
 use function Differ\Formatters\Plain\plainFormat;
+use function Differ\Formatters\Json\jsonFormat;
 
 function makeFormat(array $diff, string $formatName): string
 {
     return match ($formatName) {
         'stylish' => stylishFormat($diff),
         'plain' => plainFormat($diff),
+        'json' => jsonFormat($diff),
         default => exit("Unknown format '{$formatName}'!\n")
     };
 }
